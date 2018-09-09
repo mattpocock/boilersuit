bb29cb79f43c3d365804ab54cc5b7fb2b9e1addb
matt <matt.pocock@thevirtualforge.com>
2018-09-09 18:52:04 +0100

    Ensured commented-out imports are handled, and added reducer test generation

---
+++ commands/up/writeReducerTests.js
@@ -0,0 +1,86 @@
+const {
+  transforms,
+  prettify,
+  concat,
+  printObject,
+  ensureImport,
+  parseCamelCaseToArray,
+} = require('../../tools/utils');
+const Cases = require('../../tools/cases');
+
+module.exports = ({ buffer, cases, actions, initialState }) =>
+  transforms(buffer, [
+    ensureImport('fromJS', 'immutable', { destructure: true }),
+    ensureImport(`${cases.camel}Reducer`, '../reducer', { destructure: true }),
+    ...Object.keys(actions).map(actionName => buf => {
+      const actionCases = new Cases(parseCamelCaseToArray(actionName)).all();
+      return transforms(buf, [
+        ensureImport(actionCases.camel, '../actions', {
+          destructure: true,
+        }),
+      ]);
+    }),
+    b =>
+      concat([
+        b,
+        `// @suit-start`,
+        `describe('${cases.camel}Reducer', () => {`,
+        `  it('returns the initial state', () => {`,
+        `    expect(${cases.camel}Reducer(undefined, {}))`,
+        `      .toEqual(fromJS(${printObject(initialState, '      ')}),`,
+        `    );`,
+        `  });`,
+        ...Object.keys(actions)
+          .map(key => ({ ...actions[key], name: key }))
+          .map(action => {
+            const arrayOfSets = Object.keys(action.set)
+              .map(key => ({
+                key,
+                value: action.set[key],
+                hasPayload: `${action.set[key]}`.includes('payload'),
+              }))
+              .map(set => ({
+                ...set,
+                value:
+                  typeof set.value !== 'string' || set.hasPayload
+                    ? set.value
+                    : `'${set.value}'`,
+              }));
+            const payloadValues = arrayOfSets
+              .filter(({ hasPayload }) => hasPayload)
+              .map(({ value }) => value);
+            return concat([
+              `  describe('${action.name}', () => {`,
+              `    it('alters the state as expected', () => {`,
+              payloadValues.length === 1
+                ? `      const payload = 'dummyPayload';`
+                : null,
+              payloadValues.length > 1 ? `      const payload = {};` : null,
+              payloadValues.length > 1
+                ? concat([
+                    ...payloadValues.map(
+                      value => `      ${value} = 'dummyPayload';`,
+                    ),
+                  ])
+                : null,
+              `      const newState = ${cases.camel}Reducer(`,
+              `        undefined,`,
+              `        ${action.name}(${
+                payloadValues.length > 0 ? 'payload' : ''
+              }),`,
+              `      );`,
+              ...arrayOfSets.map(({ key, value }) =>
+                concat([
+                  `      expect(newState.get('${key}')).toEqual(${value});`,
+                ]),
+              ),
+              `    });`,
+              `  });`,
+            ]);
+          }),
+        `});`,
+        `// @suit-end`,
+        ``,
+      ]),
+    prettify,
+  ]);
--- tools/utils.js
+++ tools/utils.js
@@ -22,15 +22,18 @@ const isCapital = string => string === string.toUpperCase();
 const parseCamelCaseToArray = string =>
   string.replace(/([A-Z])/g, letter => ` ${letter}`).split(' ');
 
-const printObject = object => {
+const printObject = (object, indent = '') => {
   const newObject = JSON.stringify(object, null, 2)
     .replace(/"(\w+)"\s*:/g, '$1:')
     .replace(/"/g, "'")
     .split('\n')
     .map(line => {
       const lastChar = line[line.length - 1];
-      return (line.length === 1 || lastChar === '{' || lastChar === ',') ? line : `${line},`;
+      return line.length === 1 || lastChar === '{' || lastChar === ','
+        ? line
+        : `${line},`;
     })
+    .map((line, index) => (index === 0 ? `${line}` : `${indent}${line}`))
     .join(`\n`);
   return newObject.slice(0, -2) + newObject.slice(-2);
 };
@@ -52,72 +55,73 @@ const cleanFile = buffer => {
   return newBuffer;
 };
 
-const ensureImport = (
-  property,
-  fileName,
-  { destructure = false },
-) => buffer => {
-  /** Checks if already loaded */
-  const isImported =
-    buffer.slice(0, buffer.lastIndexOf(' from ')).indexOf(`${property}`) !== -1;
-  if (isImported) {
-    return buffer;
-  }
+const ensureImport = (property, fileName, { destructure = false }) => b =>
+  transforms(b, [
+    buffer => {
+      /** Checks if already loaded */
+      const isImported =
+        buffer.slice(0, buffer.lastIndexOf(' from ')).indexOf(`${property}`) !==
+        -1;
+      if (isImported) {
+        return buffer;
+      }
 
-  const hasImportsFromFileName = buffer.indexOf(fileName) !== -1;
+      const hasImportsFromFileName = buffer.indexOf(fileName) !== -1;
 
-  /** If no imports from fileName, add it and the filename */
-  if (!hasImportsFromFileName) {
-    const firstImportLineIndex = buffer.lastIndexOf('import');
-    if (destructure) {
-      return (
-        buffer.slice(0, firstImportLineIndex) +
-        concat([
-          `import {`,
-          `  ${property}, // @suit-line`,
-          `} from '${fileName}';`,
+      /** If no imports from fileName, add it and the filename */
+      if (!hasImportsFromFileName) {
+        const firstImportLineIndex = buffer.lastIndexOf('import');
+        if (destructure) {
+          return (
+            buffer.slice(0, firstImportLineIndex) +
+            concat([
+              `import {`,
+              `  ${property}, // @suit-line`,
+              `} from '${fileName}';`,
+              buffer.slice(firstImportLineIndex),
+            ])
+          );
+        }
+        return concat([
+          buffer.slice(0, firstImportLineIndex),
+          `import ${property} from '${fileName}'; // @suit-line`,
           buffer.slice(firstImportLineIndex),
-        ])
-      );
-    }
-    return concat([
-      buffer.slice(0, firstImportLineIndex),
-      `import ${property} from '${fileName}'; // @suit-line`,
-      buffer.slice(firstImportLineIndex),
-    ]);
-  }
-  /**
-   * Now we know that we have imports from the filename,
-   * it's just whether it's destructured or not
-   */
-  if (destructure) {
-    const isOnNewLine =
-      buffer.split('\n').findIndex(line => line === `} from '${fileName}';`) !==
-      -1;
-    if (isOnNewLine) {
-      const index = buffer.indexOf(`\n} from '${fileName}';`);
-      return (
-        buffer.slice(0, index) +
-        `\n  ${property}, // @suit-line` +
-        buffer.slice(index)
-      );
-    }
-    const index = buffer.indexOf(` from '${fileName}';`);
-    return (
-      buffer.slice(0, index) +
-      concat([`, {`, `  ${property}, // @suit-line`, `}`]) +
-      buffer.slice(index)
-    );
-  }
+        ]);
+      }
+      /**
+       * Now we know that we have imports from the filename,
+       * it's just whether it's destructured or not
+       */
+      if (destructure) {
+        const isOnNewLine =
+          buffer
+            .split('\n')
+            .findIndex(line => line === `} from '${fileName}';`) !== -1;
+        if (isOnNewLine) {
+          const index = buffer.indexOf(`\n} from '${fileName}';`);
+          return (
+            buffer.slice(0, index) +
+            `\n  ${property}, // @suit-line` +
+            buffer.slice(index)
+          );
+        }
+        const index = buffer.indexOf(` from '${fileName}';`);
+        return (
+          buffer.slice(0, index) +
+          concat([`, {`, `  ${property}, // @suit-line`, `}`]) +
+          buffer.slice(index)
+        );
+      }
 
-  /**
-   * I don't think there's a case for handling
-   * multiple default inputs from the same filename,
-   * so I'm not writing it.
-   */
-
-  return buffer;
-};
+      /**
+       * I don't think there's a case for handling
+       * multiple default inputs from the same filename,
+       * so I'm not writing it.
+       */
+      return buffer;
+    },
+    correctCommentedOutImport(fileName),
+  ]);
 
 const removeWhiteSpace = buffer => {
   const lines = buffer.split('\n');
@@ -125,6 +129,23 @@ const removeWhiteSpace = buffer => {
     .filter((thisLine, index) => !(thisLine === '' && lines[index + 1] === ''))
     .join('\n');
 };
+
+/**
+ * If an import required by boilersuit has been commented out,
+ * this corrects it
+ */
+const correctCommentedOutImport = fileName => buffer =>
+  transforms(buffer, [
+    b => {
+      const index = buffer
+        .slice(0, buffer.indexOf(`'${fileName}'`))
+        .lastIndexOf('import');
+      if (buffer.slice(index - 3, index - 1) === '//') {
+        return b.slice(0, index - 3) + b.slice(index);
+      }
+      return b;
+    },
+  ]);
 
 const removeSuitDoubling = buffer =>
   buffer
